d89a9e76a21260359255ea558981260b71d31072
Josh Luerssen <joshluer@gmail.com>
2020-05-14 16:13:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ function prependKitten(name) {
 
 function removeLastKitten() {
   return kittens.slice(0, kittens.length - 1);
-  //return kittens;
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
 
